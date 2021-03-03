ef97efd46ce0c5c7b6e3cb1fc001d213beb1f3a2
JulioArnado <77515359+JulioArnado@users.noreply.github.com>
2021-03-03 17:13:53 -0500

    Animaciones de FadeIn Agregadas
    Se agregaron animaciones de fade in

--- codigo.js
+++ codigo.js
@@ -1,25 +1,30 @@
 "use strict"; 
 //Función para agregar animaciones
-function activarAnimacion(idElemento,clase1, clase2){
-    //Capturar elemento
-    let elemento=document.getElementById(idElemento);             
-    function funcionScroll(){  
-        //Capturar las dimensiones del elemento
-        let tamanoElemento=elemento.getBoundingClientRect();
-        //Capturar la distancia del fin del elemento
-        //al borde superior de la pantalla
-        let top=tamanoElemento.top;
-        //Agregar animación si el elemento es parcialmente visible
-        if(top<=window.innerHeight){
-            elemento.classList.add(clase1);
-            elemento.classList.add(clase2);            
-            elemento.classList.add("animate__slow");            
-        }                
+function activarAnimacion(claseObjetivo,clase1, clase2){
+    //Capturar los elementos 
+    let listaElementos=document.getElementsByClassName(claseObjetivo);
+    for(let i=0;i<listaElementos.length;i++){
+        let elemento=listaElementos[i];             
+        function funcionScroll(){  
+            //Capturar las dimensiones del elemento
+            let tamanoElemento=elemento.getBoundingClientRect();
+            //Capturar la distancia del fin del elemento
+            //al borde superior de la pantalla
+            let top=tamanoElemento.top;
+            //Agregar animación si el elemento es parcialmente visible
+            if(top<=window.innerHeight){
+                elemento.classList.add(clase1);
+                elemento.classList.add(clase2);            
+                elemento.classList.add("animate__slow");            
+            }                
+        }
+        //Ajustar scroll 
+        function controlarScroll(){
+            window.requestAnimationFrame(funcionScroll);
+        }
+        //Añadir el evento scroll
+        window.addEventListener("scroll",controlarScroll);
     }
-    //Ajustar scroll 
-    function controlarScroll(){
-        window.requestAnimationFrame(funcionScroll);
-    }
-    //Añadir el evento scroll
-    window.addEventListener("scroll",controlarScroll);
+
 }
+
