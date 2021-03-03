14a29e9bbce1765ede60c4af9257c6bed2e3c0ff
JulioArnado <77515359+JulioArnado@users.noreply.github.com>
2021-03-02 22:28:20 -0500

    Actualización animaciones

---
+++ codigo.js
@@ -0,0 +1,25 @@
+"use strict"; 
+//Función para agregar animaciones
+function activarAnimacion(idElemento,clase1, clase2){
+    //Capturar elemento
+    let elemento=document.getElementById(idElemento);             
+    function funcionScroll(){  
+        //Capturar las dimensiones del elemento
+        let tamanoElemento=elemento.getBoundingClientRect();
+        //Capturar la distancia del fin del elemento
+        //al borde superior de la pantalla
+        let top=tamanoElemento.top;
+        //Agregar animación si el elemento es parcialmente visible
+        if(top<=window.innerHeight){
+            elemento.classList.add(clase1);
+            elemento.classList.add(clase2);            
+            elemento.classList.add("animate__slow");            
+        }                
+    }
+    //Ajustar scroll 
+    function controlarScroll(){
+        window.requestAnimationFrame(funcionScroll);
+    }
+    //Añadir el evento scroll
+    window.addEventListener("scroll",controlarScroll);
+}
